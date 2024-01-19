3c6283fd6a80644442a62f3b3b97dab7a25b4446
Luciamercorelli <109826365+Luciamercorelli@users.noreply.github.com>
2024-01-19 13:30:48 +0100

    display episode info and characters info

--- dist/index.js
+++ dist/index.js
@@ -10,60 +10,123 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 const urlEpisodes = "https://rickandmortyapi.com/api/episode";
 const urlCharacters = "https://rickandmortyapi.com/api/character";
 const urlLocations = "https://rickandmortyapi.com/api/location";
-function getEpisodes() {
+const episodesList = document.getElementById("episode-list");
+const nextBtn = document.getElementById("load-more");
+printTitle(urlEpisodes);
+function printTitle(url) {
     return __awaiter(this, void 0, void 0, function* () {
-        try {
-            const apiEpisode = yield fetch(urlEpisodes);
-            const data = yield apiEpisode.json();
-            const episodes = data.results;
-            episodes.forEach((episode) => {
-                console.log(episode);
-                const container = document.getElementById("episode-list");
-                const liEpisode = document.createElement("li");
-                liEpisode.classList.add("episode-list-item");
-                liEpisode.textContent = episode.name;
-                container.appendChild(liEpisode);
-            });
-            return data;
+        const data = yield fetch(url);
+        const JSONdata = yield data.json();
+        const episodes = JSONdata.results;
+        episodes.forEach((episodeElement) => {
+            episodesList.insertAdjacentHTML("beforeend", `<li id="episode${episodeElement.episode}" episodeUrl="${episodeElement.url}">${episodeElement.name}</li>`);
+            const clickEpisode = document.getElementById(`episode${episodeElement.episode}`);
+            clickEpisode.addEventListener("click", displayElementInfo);
+        });
+        if (JSONdata.info.next) {
+            nextBtn.addEventListener("click", () => {
+                printTitle(JSONdata.info.next);
+            }, { once: true });
         }
-        catch (error) {
-            throw new Error("Fail");
+        else {
+            nextBtn.remove();
         }
     });
 }
-getEpisodes().then((dataResult) => {
-    getNextEpisodes(dataResult);
-});
-function getNextEpisodes(dataResults) {
-    const loadMoreBtn = document.getElementById("load-more");
-    let checkEvent = true;
-    loadMoreBtn.addEventListener("click", () => __awaiter(this, void 0, void 0, function* () {
-        if (checkEvent) {
-            checkEvent = false;
-            displayMoreEpisodes(dataResults);
-        }
-    }));
-}
-function displayMoreEpisodes(dataResults) {
+//CLICK DISPLAY EPISODIO
+function displayElementInfo(click) {
     return __awaiter(this, void 0, void 0, function* () {
-        try {
-            if (dataResults.info.next) {
-                const response = yield fetch(dataResults.info.next);
-                const data = yield response.json();
-                const episodes = data.results;
-                episodes.forEach((episode) => {
-                    const container = document.getElementById("episode-list");
-                    const liEpisode = document.createElement("li");
-                    liEpisode.classList.add("episode-list-item");
-                    liEpisode.textContent = episode.name;
-                    container.appendChild(liEpisode);
-                });
-                return data;
-            }
-        }
-        catch (error) {
-            throw new Error("Fail");
-        }
+        const target = click.target;
+        const urlEpisode = target.getAttribute("episodeUrl");
+        const data = yield fetch(urlEpisode);
+        const episodeInfo = yield data.json();
+        const displayEpisodeInfo = `<p>${episodeInfo.name}</p>
+  <p>${episodeInfo.air_date}</p>
+  <p>${episodeInfo.episode}</p>`;
+        const printEpisodeInfo = document.getElementById("content-area");
+        printEpisodeInfo.innerHTML = displayEpisodeInfo;
+        const characters = episodeInfo.characters;
+        characters.forEach((urlCharacters) => __awaiter(this, void 0, void 0, function* () {
+            const data = yield fetch(urlCharacters);
+            const characterInfo = yield data.json();
+            const displayCharacterInfo = `<p>${characterInfo.name}</p>
+  <p>${characterInfo.status}</p>
+  <p>${characterInfo.species}</p>
+  <p>${characterInfo.gender}</p>
+  <img src=${characterInfo.image}>`;
+            printEpisodeInfo.insertAdjacentHTML("beforeend", displayCharacterInfo);
+        }));
     });
 }
 export {};
+/* import { InfoAPI, Episode } from "./interface";
+const urlEpisodes = "https://rickandmortyapi.com/api/episode";
+const urlCharacters = "https://rickandmortyapi.com/api/character";
+const urlLocations = "https://rickandmortyapi.com/api/location";
+
+async function getEpisodes(): Promise<InfoAPI> {
+  try {
+    const apiEpisode = await fetch(urlEpisodes);
+    const data: InfoAPI = await apiEpisode.json();
+    const episodes: Episode[] = data.results;
+
+    episodes.forEach((episode) => {
+      console.log(episode);
+      const container = document.getElementById(
+        "episode-list"
+      ) as HTMLUListElement;
+      const liEpisode = document.createElement("li");
+      liEpisode.classList.add("episode-list-item");
+      liEpisode.textContent = episode.name;
+      container.appendChild(liEpisode);
+    });
+
+    return data;
+  } catch (error) {
+    throw new Error("Fail");
+  }
+}
+
+getEpisodes().then((dataResult) => {
+  getNextEpisodes(dataResult);
+});
+.then((dataResults) => {
+
+})
+
+function getNextEpisodes(dataResults: InfoAPI): void {
+  const loadMoreBtn = document.getElementById("load-more") as HTMLButtonElement;
+  let checkEvent: boolean = true;
+  loadMoreBtn.addEventListener("click", async () => {
+    if (checkEvent) {
+      checkEvent = false;
+      return displayMoreEpisodes(dataResults);
+    }
+  });
+}
+
+async function displayMoreEpisodes (dataResults: InfoAPI):Promise<InfoAPI> {
+  try {
+   
+      const response = await fetch(dataResults.info.next);
+      const data: InfoAPI = await response.json();
+      const episodes: Episode[] = data.results;
+
+
+      episodes.forEach((episode) => {
+        const container = document.getElementById(
+          "episode-list"
+        ) as HTMLUListElement;
+        const liEpisode = document.createElement("li");
+        liEpisode.classList.add("episode-list-item");
+        liEpisode.textContent = episode.name;
+        container.appendChild(liEpisode);
+      });
+      return data;
+    }
+
+
+  } catch (error) {
+    throw new Error("Fail");
+  }
+} */
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,81 @@
-import { InfoAPI, Episode } from "./interface";
+import { InfoAPI, Episode, Character } from './interface';
+const urlEpisodes = "https://rickandmortyapi.com/api/episode";
+const urlCharacters = "https://rickandmortyapi.com/api/character";
+const urlLocations = "https://rickandmortyapi.com/api/location";
+
+const episodesList = document.getElementById(
+  "episode-list"
+) as HTMLUListElement;
+const nextBtn = document.getElementById("load-more") as HTMLButtonElement;
+
+printTitle(urlEpisodes);
+
+async function printTitle(url: string) {
+  const data = await fetch(url);
+  const JSONdata: InfoAPI = await data.json();
+  const episodes: Episode[] = JSONdata.results;
+
+  episodes.forEach((episodeElement) => {
+    episodesList.insertAdjacentHTML(
+      "beforeend",
+      `<li id="episode${episodeElement.episode}" episodeUrl="${episodeElement.url}">${episodeElement.name}</li>`
+    );
+    const clickEpisode = document.getElementById(
+      `episode${episodeElement.episode}`
+    ) as HTMLLIElement;
+    clickEpisode.addEventListener("click", displayElementInfo);
+  });
+
+  if (JSONdata.info.next) {
+    nextBtn.addEventListener(
+      "click",() => {
+        printTitle(JSONdata.info.next);
+      },
+      { once: true }
+    );
+  } else {
+    nextBtn.remove();
+  }
+}
+
+
+//CLICK DISPLAY EPISODIO
+async function displayElementInfo(click:MouseEvent) {
+  const target = click.target as HTMLLIElement;
+  const urlEpisode = target.getAttribute("episodeUrl")!;
+  const data = await fetch(urlEpisode);
+  const episodeInfo: Episode = await data.json();
+  const displayEpisodeInfo = 
+  `<p>${episodeInfo.name}</p>
+  <p>${episodeInfo.air_date}</p>
+  <p>${episodeInfo.episode}</p>`;
+  
+  const printEpisodeInfo = document.getElementById("content-area") as HTMLDivElement;  
+  printEpisodeInfo.innerHTML =displayEpisodeInfo;  
+  const characters = episodeInfo.characters
+  characters.forEach(async urlCharacters => {
+  const data = await fetch(urlCharacters);  
+  const characterInfo: Character = await data.json(); 
+    const displayCharacterInfo = 
+  `<p>${characterInfo.name}</p>
+  <p>${characterInfo.status}</p>
+  <p>${characterInfo.species}</p>
+  <p>${characterInfo.gender}</p>
+  <img src=${characterInfo.image}>`;
+  printEpisodeInfo.insertAdjacentHTML("beforeend",displayCharacterInfo);
+  });
+}
+
+
+
+
+
+
+
+
+
+
+/* import { InfoAPI, Episode } from "./interface";
 const urlEpisodes = "https://rickandmortyapi.com/api/episode";
 const urlCharacters = "https://rickandmortyapi.com/api/character";
 const urlLocations = "https://rickandmortyapi.com/api/location";
@@ -29,6 +106,9 @@ async function getEpisodes(): Promise<InfoAPI> {
 getEpisodes().then((dataResult) => {
   getNextEpisodes(dataResult);
 });
+.then((dataResults) => {
+
+})
 
 function getNextEpisodes(dataResults: InfoAPI): void {
   const loadMoreBtn = document.getElementById("load-more") as HTMLButtonElement;
@@ -36,17 +116,18 @@ function getNextEpisodes(dataResults: InfoAPI): void {
   loadMoreBtn.addEventListener("click", async () => {
     if (checkEvent) {
       checkEvent = false;
-      displayMoreEpisodes(dataResults);
+      return displayMoreEpisodes(dataResults);
     }
   });
 }
 
-async function displayMoreEpisodes(dataResults: InfoAPI) {
+async function displayMoreEpisodes (dataResults: InfoAPI):Promise<InfoAPI> {
   try {
-    if (dataResults.info.next) {
+   
       const response = await fetch(dataResults.info.next);
       const data: InfoAPI = await response.json();
       const episodes: Episode[] = data.results;
+
 
       episodes.forEach((episode) => {
         const container = document.getElementById(
@@ -59,7 +140,9 @@ async function displayMoreEpisodes(dataResults: InfoAPI) {
       });
       return data;
     }
+
+
   } catch (error) {
     throw new Error("Fail");
   }
-}
+} */
